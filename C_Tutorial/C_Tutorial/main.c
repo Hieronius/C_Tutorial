e8436e41c04f81563ae0a78946ba3bd0befb7c54
Arsenty Halimovsky <ghost12@yandex.ru>
2023-12-28 13:27:00 +0300

    Create a new project

---
+++ C_Tutorial/C_Tutorial/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_Tutorial
+//
+//  Created by Арсентий Халимовский on 28.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
